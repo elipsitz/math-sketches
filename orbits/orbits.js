189473f91524272a61cb30adcd85c22422a75922
aegamesi <admin.aegamesi@gmail.com>
2015-11-23 10:22:50 -0600

    Added basic bodies, rendering, gravity

--- orbits/orbits.js
+++ orbits/orbits.js
@@ -7,76 +7,72 @@ var canvas_mousey = 0;
 var canvas_mousedownx = 0;
 var canvas_mousedowny = 0;
 
-function redraw() {
+var graph_width = 800;
+var graph_height = 600;
+var time = 0;
+var time_scale = 1;
+
+var const_g = 1;//6.67e-11;
+var camera_x = 0;
+var camera_y = 0;
+var camera_zoom = 1;
+var objects = [];
+
+// starter object
+objects = [
+	{x: 0, y: 0, mass: 100000, radius: 10, vx: 0, vy: 0, ax: 0, ay: 0},
+	{x: 300, y: 0, mass: 100000, radius: 5, vx: 0, vy: -10, ax: 0, ay: 0}
+];
+
+function render() {
+	var dt =  (1.0 / 60.0) * time_scale;
+	time += dt;
 	ctx.clearRect(0, 0, graph_width, graph_height);
-	
-	// draw in the control points/control path
-	ctx.fillStyle = 'lightGray';
-	ctx.strokeStyle = 'lightGray';
-	for(var i = 0; i < control_points.length; i++) {
+
+	// simulate objects (TODO)
+	for(var i = 0; i < objects.length; i++) {
+		// first compute their acceleration vectors
+		objects[i].ax = 0;
+		objects[i].ay = 0;
+		for(var j = 0; j < objects.length; j++) {
+			if(i == j)
+				continue;
+			var rx = objects[i].x - objects[j].x;
+			var ry = objects[i].y - objects[j].y;
+			var r = -(const_g * objects[j].mass) / Math.pow(Math.sqrt(rx * rx + ry * ry), 3);
+
+			objects[i].ax += r * rx;
+			objects[i].ay += r * ry;
+		}
+
+		// now velocity
+		objects[i].vx += objects[i].ax * dt;
+		objects[i].vy += objects[i].ay * dt;
+
+		// and position
+		objects[i].x += objects[i].vx * dt;
+		objects[i].y += objects[i].vy * dt;
+	}
+
+	// camera position
+	if(objects.length > 0) {
+		camera_x = objects[0].x - (graph_width * 0.5);
+		camera_y = objects[0].y - (graph_height * 0.5);
+	}
+
+	// draw objects
+	ctx.fillStyle = 'black';
+	for(var i = 0; i < objects.length; i++) {
+		var x = objects[i].x - camera_x;
+		var y = objects[i].y - camera_y;
 		ctx.beginPath();
-		ctx.arc(control_points[i].x, control_points[i].y, 2, 0, Math.PI * 2);
+		ctx.arc(x, y, objects[i].radius * camera_zoom, 0, Math.PI * 2);
 		ctx.fill();
+
+		line(x, y, x + objects[i].vx, y + objects[i].vy);
 	}
-	ctx.beginPath();
-	for(var i = 0; i < control_points.length; i++) {
-		if(i == 0)
-			ctx.moveTo(control_points[i].x, control_points[i].y);
-		ctx.lineTo(control_points[i].x, control_points[i].y);
-	}
-	ctx.stroke();
-	
-	// now, draw in current progress
-	//ctx.strokeStyle = 'black';
-	//ctx.beginPath();
-	var points = control_points.slice(0);
-	while(points.length > 1) {
-		ctx.strokeStyle = graph_colors[points.length % graph_colors.length];
-		ctx.beginPath();
-		var next_arr = [];
-		for(var i = 0; i < points.length - 1; i++) {
-			var new_pt = lerp_pt(points[i], points[i+1], t);
-			next_arr.push(new_pt);
-			console.log(new_pt);
-			if(i == 0)
-				ctx.moveTo(new_pt.x, new_pt.y);
-			ctx.lineTo(new_pt.x, new_pt.y);
-			/*if(i > 0)
-				line(next_arr[i - 1].x, next_arr[i - 1].y, new_pt.x, new_pt.y);*/
-			/*if(i > 0) {
-				ctx.moveTo(next_arr[i - 1].x, next_arr[i - 1].y);
-				ctx.lineTo(new_pt.x, new_pt.y);
-				ctx.stroke();
-			}*/
-		}
-		ctx.stroke();
-		points = next_arr;
-	}
-	ctx.stroke();
-	// draw a point
-	ctx.fillStyle = 'red';
-	if(points.length == 1) {
-		if(t < 1.0) {
-			ctx.beginPath();
-			ctx.arc(points[0].x, points[0].y, 4, 0, Math.PI * 2);
-			ctx.fill();
-		}
-		rendered_points[(t * 100) | 0] = points[0];
-	}
-	
-	// now render rendered points
-	ctx.strokeStyle = 'red';
-	ctx.beginPath();
-	var drawn = false;
-	for(var i = 0; i < (t * 100); i++) {
-		if(rendered_points[i] == undefined)
-			continue;
-		if(!drawn)
-			ctx.moveTo(rendered_points[i].x, rendered_points[i].y);
-		ctx.lineTo(rendered_points[i].x, rendered_points[i].y);
-		drawn = true;
-	}
-	ctx.stroke();
+
+	window.requestAnimFrame(render, canvas);
 }
 
 function line(x0, y0, x1, y1){
@@ -122,6 +118,17 @@ function getMousePos(canvas, evt) {
     };
 }
 
+window.requestAnimFrame = (function() {
+	return window.requestAnimationFrame ||
+		window.webkitRequestAnimationFrame ||
+		window.mozRequestAnimationFrame ||
+		window.oRequestAnimationFrame ||
+		window.msRequestAnimationFrame ||
+		function( /* function FrameRequestCallback */ callback, /* DOMElement Element */ element) {
+			return window.setTimeout(callback, 1000 / 60);
+		};
+})();
+
 $(document).ready(function(){
 	canvas = $('#canvas')[0];
     ctx = canvas.getContext('2d');
@@ -129,31 +136,11 @@ $(document).ready(function(){
 	// begin jQuery Event Handlers
 	$('#slider_percent').on('input change', function(){
 		t = $(this).val() / 100.0;
-		animating = false;
-		redraw();
+		time_scale = t * 10.0;
 	});
 	$('#button_clear').click(function() {
-		animating = false;
-		control_points = [];
-		rendered_points = [];
-		redraw();
 	});
 	$('#button_go').click(function() {
-		redraw();
-		
-		t = 0.0;
-		animating = true;
-		var intervalId = setInterval(function(){
-			t += 0.01;
-			$('#slider_percent').val(t * 100);
-			redraw();
-
-			
-			if(!animating || t >= 1.0) {
-				redraw();
-				clearInterval(intervalId);
-			}
-		}, 20);
 	});
 	
 	$('#canvas').mousedown(function(e) {
@@ -163,10 +150,6 @@ $(document).ready(function(){
 		canvas_mousedowny = pos.y;
 		canvas_mousex = pos.x;
 		canvas_mousey = pos.y;
-		
-		control_points.push({x: canvas_mousex, y: canvas_mousey});
-		rendered_points = [];
-		redraw();
 	});
 	$(window).mouseup(function(e) {
 		canvas_mousedown = false;
@@ -182,5 +165,5 @@ $(document).ready(function(){
 	// end jQuery Event Handlers
 	
 	// Actual code to run on DOM Launch
-	redraw(); 
+	render();
 });
